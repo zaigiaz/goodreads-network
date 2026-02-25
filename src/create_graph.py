f6d68389be5dce6ae0baddddd35be37d50610149
zaigiaz <shayne.tman@gmail.com>
2026-02-25 14:35:37 -0600

    csv_reader object iterator fixing, structured code better

--- src/create_graph.py
+++ src/create_graph.py
@@ -1,9 +1,32 @@
 import networkx as nx
 import csv
+import copy
+
+# main file for my goodreads network
 
 
-with open("../data/goodreads_books_dataset.csv") as csv_file:
-    csv_read = csv.reader(csv_file, delimiter=',')
+def data_report(csv_read):
     print("length of csv file", len(list(csv_read)))
+
+
+def open_data(filename):
+    with open(filename, newline='') as csv_file:
+        csv_read = csv.reader(csv_file, delimiter=',', quotechar='"')
+
+        for row in csv_read:
+            print(row)
+
+
+        csv_file.seek(0)
+        reader = csv.reader(csv_file)
+
+        data_report(csv_read)
+
+
+                                
     
+
+open_data('../data/goodreads_books_dataset.csv')
+    
+
 
